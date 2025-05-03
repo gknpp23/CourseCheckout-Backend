a650c2156c1fe0c25db782adfedffc4e551487a3
Gabriel Knupp <gabrielknpp@gmail.com>
2025-05-03 19:29:09 -0300

    feat: Adicionei rotas de pagamento no index.js

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -4,12 +4,12 @@ const router = express.Router();
 
 // Importar suas rotas específicas
 const studentRoutes = require('./student.routes');
-// const paymentRoutes = require('./api/v1/payment.routes');
+const paymentRoutes = require('./payment.routes');
 // const healthRoutes = require('./api/v1/health.routes');
 
 // Configurar as rotas
 router.use('/students', studentRoutes);
-// router.use('/payments', paymentRoutes);
+router.use('/payments', paymentRoutes);
 // router.use('/health', healthRoutes);
 
 module.exports = router; // ← Exporta o router configurado
