901ae0bf0aef274d1b14e010a6a80df1c7454d06
Gabriel Knupp <gabrielknpp@gmail.com>
2025-05-21 23:45:50 +0000

    refactor: limpa comentários obsoletos

--- backend/src/routes/payment.routes.js
+++ backend/src/routes/payment.routes.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const { post } = require('axios');
-const Student = require('../db/models/Student.js'); // Ajuste o caminho conforme sua estrutura
+const Student = require('../db/models/Student.js'); 
 const asyncHandler = require('../middlewares/asyncHandler.js'); // Middleware para tratamento de erros
-const { sendEmail } = require('../services/email.services'); // Corrigido: importação ausente
+const { sendEmail } = require('../services/email.services'); 
 
 const router = express.Router();
 
